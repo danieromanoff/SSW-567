684e6271a65b9d74ac877a3a01e435076222fa46
Danielle <dlromanoff@gmail.com>
2017-01-21 19:24:29 -0500

    Update TriangleClass.java

--- TriangleSides/src/TriangleClass.java
+++ TriangleSides/src/TriangleClass.java
@@ -1,85 +1,56 @@
 
-class classifyTriangle {
+public class TriangleSides
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+ 
+        int side1, side2, side3;
+ 
+        System.out.println("\nTo determine the type of triangle please enter the lengths of the sides. " +
+                           "\nEach length MUST be entered in INCREASING order of size.\n");
+ 
+        System.out.println("Side 1: ");
+        side1 = keyboard.nextInt();
+ 
+        System.out.println("Side 2: ");
+        side2 = keyboard.nextInt();
+ 
+        System.out.println("Side 3: ");
+        side3 = keyboard.nextInt();
+ 
+        //To check to see if it is an triangle
+       // The sum of two smaller sides must be greater than the longest side to be a triangle
+        if((side1 + side2 > side3))
+        {
+            //To check to see if it an Equilateral triangle (3 equal sides)
+            if((side1==side2) && (side2==side3))
+            {
+               System.out.print("\n RESULT: An Equilateral Triangle.");
+            }//if
+ 
+            //To check to see if it is an Isosceles triangle (2 of the same size and 1 different)
+            else if ((side1 == side2) & (side2 != side3) || (side2 == side3) & (side3!= side1))
+            {
+                System.out.print("\n RESULT: An Isosceles Triangle.");
+            }//else if
+ 
+                 //To check to see if it is a Scalene triangle (3 different sizes)
+                 else if((side1 != side2) & (side2 != side3))
+                 {
+                    System.out.print("\n RESULT: A Scalene Triangle.");
+                 }//else if
+ 
+        }//if
+ 
+        //Sum of two smaller sides is smaller than the longest side
+        //When it is not a triangle it will print this.
+        else
+        {
+            System.out.print("\n RESULT: This Triangle is NOT possible.");
+        }//else
+ 
+ 
+    }//main
+}//class
 
-   // Stores the length of each side of the Triangle object.
-   private int side1, side2, side3;
-
-   // Constructor to initialize the sides of the triangle.
-   public classifyTriangle (int s1, int s2, int s3) {
-      side1 = s1;
-      side2 = s2;
-      side3 = s3;
-   }
-
-   // Method to test for a right-angled triangle.
-   public boolean is_right () {
-      if (((side1*side1) == ((side2*side2) + (side3*side3))) ||
-          ((side2*side2) == ((side1*side1) + (side3*side3))) ||
-          ((side3*side3) == ((side1*side1) + (side2*side2))))
-         return true;
-      else
-         return false;
-   }
-
-   // Method to test for a scalene triangle.
-   public boolean is_scalene () {
-      if ((side1 != side2) && (side1 != side3) && (side2 != side3))
-         return true;
-      else
-         return false;
-   }
-
-   // Method to test for an isosceles triangle.
-   public boolean is_isosceles () {
-      if (((side1 == side2) && (side1 != side3)) ||
-          ((side1 == side3) && (side1 != side2)) ||
-          ((side2 == side3) && (side2 != side1)))
-         return true;
-      else
-         return false;
-   }
-
-   // Method to test for an equilateral triangle.
-   public boolean is_equilateral () {
-      if ((side1 == side2) && (side1 == side3))
-         return true;
-      else
-         return false;
-   }
-}
-public static void main (String[] args) throws IOException {
-
-      // Declare stdin so data can be read from input.
-      DataInputStream stdin = new DataInputStream (System.in);
-
-      // loop exits when the user response is "n"
-      String entries = "y";
-      while (!entries.equals("n")) {
-
-         // Ask user for 3 dimensions of triangle.
-         System.out.println ("\nEnter side1 length: ");
-         int side1 = Integer.parseInt (stdin.readLine());
-
-         System.out.println ("Enter side2 length: ");
-         int side2 = Integer.parseInt (stdin.readLine());
-
-         System.out.println ("Enter side3 length: ");
-         int side3 = Integer.parseInt (stdin.readLine());
-
-         // Now use these values to create a Triangle object.
-         Triangle tri = new Triangle (side1,side2,side3);
-
-         // Determine what kind of triangle it is.
-         System.out.println ("\nIs triangle right-angle? "+tri.is_right());
-         System.out.println ("Is triangle scalene?     "+tri.is_scalene());
-         System.out.println ("Is triangle isosceles?   "+
-                                                       tri.is_isosceles());
-         System.out.println ("Is triangle equilateral? "+
-                                                     tri.is_equilateral());
-         // Ask user if they want to continue.
-         System.out.println ("\nDo you want to examine more triangles?");
-         System.out.println ("(type 'y' for yes or 'n' for no)");
-         user_response = stdin.readLine();
-      }
-   }
-}
